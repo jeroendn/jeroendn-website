63d4ade962c718fead54c26b14ece3eb1a24aaf2
Jeroen de Nijs <jeroendenijs.k.smile@outlook.com>
2022-10-30 11:30:02 +0100

    V1.2.3

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-## Changelog V1.2.2 ##
-Date published: 20 Feb 2022
+## Changelog V1.2.3 ##
+Date published: 30 Oct 2022
 
 ### New features & changes ###
-- Updated projects to have the db password configured in the .env file.
+- Dependency updates.
 
 ### Bug fixes ###
-<em>No entry</em>
+- None
 
 ## Version update procedure ##
-1. Check README contents and version/date
-2. Upload to webserver
-3. Commit to Git
-4. Clear README contents and version/date
+1. Check README contents and version/date.
+2. Upload to webserver.
+3. Commit to Git.
+4. Clear README contents and version/date.
 
 ## New development environment ##
-1. Run command: composer install
-2. Run command: npm install
-3. Use the command 'npm run watch' to compile SCSS and JS
+1. Run command: ```composer install```.
+2. Run command: ```npm install```.
+3. Use the command ```npm run watch``` to compile SCSS and JS.
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,15 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/script.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/style.scss', 'public/css')
-    .sass('resources/sass/tools/tools.scss', 'public/css')
-    .sass('resources/sass/diary/diary.scss', 'public/css')
-    .sourceMaps(true, 'source-map');
-
+mix
+.js('resources/js/app.js', 'public/js')
+.js('resources/js/script.js', 'public/js')
+.sass('resources/sass/app.scss', 'public/css')
+.sass('resources/sass/style.scss', 'public/css')
+.sass('resources/sass/tools/tools.scss', 'public/css')
+.sass('resources/sass/diary/diary.scss', 'public/css')
+.sourceMaps(false, 'source-map')
+.webpackConfig({
+    stats: {
+        // children: true // Show errors
+    }
+});
